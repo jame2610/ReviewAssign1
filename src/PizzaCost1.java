fc683129d90d5c3ece6c0745df8842b473fad06f
jame2610 <jame2610@ISS2V2PKH2.scdsb.pri>
2020-09-15 12:35:44 -0400

    Worked on pizza 1

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -9,6 +9,15 @@ public class PizzaCost1 {
         Scanner s = new Scanner(System.in);
         final double labour = 1.5;
         final double heat =0.75;
+        final double tops = 0.75;
+        int inchp, pizza;
+        
+        System.out.println("Enter the number of toppings >> ");
+        
+        System.out.println("Enter size of Pizza in inches >> ");
+        inchp = s.nextInt();
+        
+        
     }
     
 }
